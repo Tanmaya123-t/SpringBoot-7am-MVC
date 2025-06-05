cfe1dc8c14bb52ce1bccb2cb46e64adc665beb1a
tanmaya <tanmaya@git.com>
2025-06-05 17:08:59 +0530

    second commit

---
+++ src/main/java/com/nt/SpringBoot7amMvcApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot7amMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot7amMvcApplication.class, args);
+	}
+
+}
